67350dda231f911f204d753959c9029e14c14cbc
alexpp9 <alexandrup.pirtac@outlook.com>
2024-07-07 14:01:34 +0100

    modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,24 @@
+class Modal {
+  constructor() {
+    this._modal = document.querySelector('#modal');
+    this._modalBtn = document.querySelector('#modal-btn');
+    this.addEventLisiteners();
+  }
+  addEventLisiteners() {
+    this._modalBtn.addEventListener('click', this.open.bind(this));
+    window.addEventListener('click', this.outsideClick.bind(this));
+  }
+  open() {
+    this._modal.style.display = 'block';
+  }
+  close() {
+    this._modal.style.display = 'none';
+  }
+  outsideClick(e) {
+    if (e.target === this._modal) {
+      this.close();
+    }
+  }
+}
+
+export default Modal;
